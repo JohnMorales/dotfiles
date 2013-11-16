df2e254f9103997caa17bc68fdbb73e5e490960b
JohnMorales <jmorales@gmail.com>
2013-11-15 19:31:47 -0500

    fixing setup

--- .setup/makesetup.sh
+++ .setup/makesetup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-curl -Sso ~/.vim/autoload/pathogen.vim \
+VIMPLUGINDIR=~/.vim/bundle
+mkdir -p ~/.vim/autoload $VIMPLUGINDIR;\
+curl -Sso ~/.vim/autoload/pathogen.vik \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-mkdir -p .vim/bundle
-test -f .vim/bundle/ctrlp.vim || git clone https://github.com/kien/ctrlp.vim.git .vim/bundle/ctrlp.vim
+mkdir -p $VIMPLUGINDIR
+test -d $VIMPLUGINDIR/ctrlp.vim || git clone https://github.com/kien/ctrlp.vim.git $VIMPLUGINDIR/ctrlp.vim
+test -d $VIMPLUGINDIR/jellybeans.vim || git clone https://github.com/nanotech/jellybeans.vim.git $VIMPLUGINDIR/jellybeans.vim
