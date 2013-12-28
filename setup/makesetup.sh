5042642d09b2e9141f550e8c46c009e26dd07d1f
JohnMorales <jmorales@gmail.com>
2013-12-27 21:26:05 -0500

    moved setup.sh file

--- setup/makesetup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VIMPLUGINDIR=~/.vim/bundle
-mkdir -p ~/.vim/autoload $VIMPLUGINDIR;\
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-mkdir -p $VIMPLUGINDIR
-test -d $VIMPLUGINDIR/ctrlp.vim || git clone https://github.com/kien/ctrlp.vim.git $VIMPLUGINDIR/ctrlp.vim
-test -d $VIMPLUGINDIR/jellybeans.vim || git clone https://github.com/nanotech/jellybeans.vim.git $VIMPLUGINDIR/jellybeans.vim
-test -d $VIMPLUGINDIR/nerdtree.vim || git clone https://github.com/scrooloose/nerdtree.git $VIMPLUGINDIR/nerdtree.vim
