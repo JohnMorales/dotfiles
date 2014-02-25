a7722057effc1b9c191b13f723c8840dc25e6d97
JohnMorales <jmorales@gmail.com>
2014-02-25 09:20:43 -0500

    Changing to my fork of bootstrap snippets

--- makesetup.sh
+++ makesetup.sh
@@ -26,7 +26,7 @@ test -d $VIMPLUGINDIR/vim-surround.vim || git clone https://github.com/tpope/vim
 test -d $VIMPLUGINDIR/molokai.vim || git clone https://github.com/tomasr/molokai.git $VIMPLUGINDIR/molokai.vim
 test -d $VIMPLUGINDIR/html5.vim || git clone https://github.com/othree/html5.vim.git $VIMPLUGINDIR/html5.vim
 test -d $VIMPLUGINDIR/vim-airline.vim || git clone https://github.com/bling/vim-airline.git $VIMPLUGINDIR/vim-airline.vim
-test -d $VIMPLUGINDIR/bootstrap-snippets.vim || git clone https://github.com/bonsaiben/bootstrap-snippets.git $VIMPLUGINDIR/bootstrap-snippets.vim
+test -d $VIMPLUGINDIR/bootstrap-snippets.vim || git clone https://github.com/JohnMorales/bootstrap-snippets.git $VIMPLUGINDIR/bootstrap-snippets.vim
 
 
 link_config_file() {
