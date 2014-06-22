feacabd92b18ab87c1571fd416cdcc998dffe284
JohnMorales <john.morales@exelisinc.com>
2014-06-22 15:19:58 -0400

    Adding ansi color name to display

--- show_colors.sh
+++ show_colors.sh
@@ -9,15 +9,22 @@
 #
 
 T='gYw'   # The test text
+# Ascii escape char is 27, Hex \x1B, Octal 033
 
+COLOR_NAMES=('' Black Red Green Yellow Blue Magenta Cyan White)
+LIGHT_DARK=('' Bright)
 echo -e "\n                 40m     41m     42m     43m\
      44m     45m     46m     47m";
-
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
-           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  echo -en " $FGs \033[$FG  $T  "
+FGS=('    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+     '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+     '  36m' '1;36m' '  37m' '1;37m')
+for i in "${!FGS[@]}"
+  do
+  FG=${FGS[$i]// /}
+  c=$((i/2))
+  if [ $i -gt 2 ]; then l=$((i-2)); fi
+  printf -v COLOR "%15s" "${LIGHT_DARK[$((l%2))]} ${COLOR_NAMES[$c]}"
+  echo -en " $COLOR ${FGS[$i]} \033[$FG  $T  "
   for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
     do echo -en " \033[$FG\033[$BG  $T  \033[0m";
   done
