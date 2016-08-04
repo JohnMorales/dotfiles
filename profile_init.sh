435c983da5885536ee4632306a7bf3194892813d
John Morales <jmorales@gmail.com>
2016-08-04 08:42:17 -0400

    Fixing init to check if directory already exists, fixing bash completion

--- profile_init.sh
+++ profile_init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+[ -d ~/.rbenv ] || git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-~/.rbenv/bin/rbenv install 2.2.2
+[ -d ~/.rbenv/plugins/ruby-build ] || ggit clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$')
+echo "Latest matz ruby: $ruby_install_version"
+~/.rbenv/bin/rbenv install $ruby_install_version
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
-~/.rbenv/bin/rbenv global 2.2.2
+~/.rbenv/bin/rbenv global $ruby_install_version
 ~/.rbenv/shims/gem install chef
