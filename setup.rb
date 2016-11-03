91244f31b1822a136c72faad8c902b9651726664
JohnMorales <john.morales@harris.com>
2016-11-03 14:33:00 -0400

    Removing init vim

--- setup.rb
+++ setup.rb
@@ -27,11 +27,6 @@ development_dir = File.expand_path("~/Development")
     group current_user
   end
 end
-link File.expand_path("~/.config/nvim/init.vm") do
-  to File.join(dotfiles_dir, "nvimrc")
-  owner current_user
-  group current_user
-end
 # update homebrew
 execute "update homebrew" do
   command "brew update"
