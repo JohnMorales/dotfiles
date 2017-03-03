c24a010f3032d382d7a20d0981e5207696545ebe
John Morales <jmorales@gmail.com>
2017-03-03 05:59:53 +0000

    Adding rbenv to current script

--- profile_init.sh
+++ profile_init.sh
@@ -2,6 +2,7 @@
 [ -d ~/.rbenv ] || git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 [ -d ~/.rbenv/plugins/ruby-build ] || git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+eval "$(rbenv init -)"
 ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$')
 echo "Latest matz ruby: $ruby_install_version"
 ~/.rbenv/bin/rbenv install $ruby_install_version
