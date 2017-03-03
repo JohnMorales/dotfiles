587be9d38c19e1b7f49a70466a149fed7eaebd53
John Morales <jmorales@gmail.com>
2017-03-03 05:58:05 +0000

    Fixing typo

--- profile_init.sh
+++ profile_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ -d ~/.rbenv ] || git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-[ -d ~/.rbenv/plugins/ruby-build ] || ggit clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+[ -d ~/.rbenv/plugins/ruby-build ] || git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$')
 echo "Latest matz ruby: $ruby_install_version"
 ~/.rbenv/bin/rbenv install $ruby_install_version
