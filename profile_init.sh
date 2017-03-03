a579e2c773b9fee875cd898c9fb4867f73a4beab
John Morales <jmorales@gmail.com>
2017-03-03 06:04:26 +0000

    Sourcing bashrc incase that rbenv was just installed

--- profile_init.sh
+++ profile_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 [ -d ~/.rbenv ] || git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+. ~/.bashrc
 [ -d ~/.rbenv/plugins/ruby-build ] || git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-eval "$(rbenv init -)"
 ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$'| tail -1)
 echo "Latest matz ruby: $ruby_install_version"
 ~/.rbenv/bin/rbenv install $ruby_install_version
