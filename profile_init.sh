b887d7276b381aea0394a072d894714f71efafa6
John Morales <jmorales@gmail.com>
2017-03-03 06:02:07 +0000

    Using latest ruby version

--- profile_init.sh
+++ profile_init.sh
@@ -3,7 +3,7 @@
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc && echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 [ -d ~/.rbenv/plugins/ruby-build ] || git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 eval "$(rbenv init -)"
-ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$')
+ruby_install_version=$(rbenv install -l | sed 's/^ *//' | grep ^[[:digit:]] | grep '\.[[:digit:]]$'| tail -1)
 echo "Latest matz ruby: $ruby_install_version"
 ~/.rbenv/bin/rbenv install $ruby_install_version
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
